8bbb362b6bba2be1800884f4edda2f8ea208afe1
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2022-10-06 11:02:57 +0530

    Add files via upload

---
+++ C PROGRAM/INT.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int testinteger;
+clrscr();
+printf("enter an integer:");
+scanf("%d",&testinteger);
+printf("number=%d",testinteger);
+getch();
+}
