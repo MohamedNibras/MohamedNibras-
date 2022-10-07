17c4d56a437c588bc57dd256654ae0c12bca9d99
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2022-10-07 15:03:50 +0530

    Add files via upload

---
+++ C PROGRAM/MVFLOATD.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float num1;
+double num2;
+printf("enter a number:");
+scanf("%f",&num1);
+printf("enter another number:");
+scanf("%lf",&num2);
+printf("num1=%f\n",num1);
+printf("num2=%lf",num2);
+getch();
+}
