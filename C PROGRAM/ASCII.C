71daaba34b90f5b8c27592b614eb146b2eafd808
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2022-10-07 12:38:12 +0530

    Add files via upload

---
+++ C PROGRAM/ASCII.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char chr;
+printf("enter a character:");
+scanf("%c",&chr);
+printf("you entered %c.\n",chr);
+printf("ASCII value is %d.",chr);
+getch();
+}
