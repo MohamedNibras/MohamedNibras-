5eab2b571b44a58ec9d8605590e6109df18f4825
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2022-10-07 12:47:07 +0530

    Add files via upload

---
+++ C PROGRAM/MUTIPLRV.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+float b;
+printf("enter integer and then a float:");
+scanf("%df",&a,&b);
+printf("you entered %d and %f",a,b);
+getch();
+}
