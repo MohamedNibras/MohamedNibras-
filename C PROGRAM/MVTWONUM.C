c0432580ec03a6e897733677dd1cbcf723627f45
MohamedNibras <113166568+MohamedNibras@users.noreply.github.com>
2022-10-07 14:46:01 +0530

    Add files via upload

---
+++ C PROGRAM/MVTWONUM.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter two numbers to add\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("sum of entered numbers=%d\n",c);
+getch();
+}
